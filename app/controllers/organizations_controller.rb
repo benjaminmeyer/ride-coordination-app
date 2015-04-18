035fdc81a355460ab5b3d92006c4f278aae78d6c
benjaminmeyer <Ben@Bens-MBP.home>
2015-04-18 15:32:42 -0400

    pull organizations from the database

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -3,6 +3,7 @@ class OrganizationsController < ApplicationController
 	#layout false
 	
 	def index
+		@organizations = Organization.all.active
 	end
 	
 	def view
--- app/models/organization.rb
+++ app/models/organization.rb
@@ -5,4 +5,9 @@ class Organization < ActiveRecord::Base
 	has_many :members
 	has_many :users, :through => :members
 	
+	scope :active, lambda {where(:deleted => 0)}
+	#scope :byuser, lambda {|user|
+	#	where (:user_id => user)
+	#}
+	
 end
